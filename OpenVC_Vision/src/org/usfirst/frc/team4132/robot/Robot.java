36601068ca58da0c6f76bb2bf680fd30e518956a
Thomas Eblen <tommyeblen@gmail.com>
2017-02-13 17:54:16 -0800

    added vertical line test to detect goal

--- OpenVC_Vision/src/org/usfirst/frc/team4132/robot/Robot.java
+++ OpenVC_Vision/src/org/usfirst/frc/team4132/robot/Robot.java
@@ -82,20 +82,12 @@ public class Robot extends IterativeRobot {
             
             while(!Thread.interrupted()) {
             	
-            	/*
-            	if(buttonPressed){
-            		cvSink=CameraServer.getInstance().getVideo("cam1");
-            		System.out.println("doing cam1");
-            	}
-            	else{
-            		cvSink=CameraServer.getInstance().getVideo("cam0");
-            	}
-            	*/
+            	
             	
                 cvSink.grabFrame(source);
                 Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
                 
-                //100, 150, 100 
+                
                 double color[];
                 
                 ///length must be a multiple of 3 for a rgb mat
@@ -103,59 +95,61 @@ public class Robot extends IterativeRobot {
                 int size=(int)(source.total()*source.channels());
 
                 byte[] pixels=new byte[size];
-                int pixelValues[][]=new int[output.rows()][output.cols()];
                 output.get(0, 0,pixels);
                 
                 
-                ///bgr , -1 is the lowest value
-                /*
-                System.out.print(convertColor(pixels[450000])+" ");
-                System.out.print(convertColor(pixels[450001])+" ");
-                System.out.print(convertColor(pixels[450002])+" ");
-                System.out.print("\n");
-                */
+              
                 
-                /*
-                for(int x=0; x<pixels.length; x+=3){
-                	//double d=get3DDistanceSqr(BLUE[0], BLUE[1], BLUE[2], convertColor(pixels[450000]), convertColor(pixels[450001]), convertColor(pixels[450002]));
+                
+                
+                
+                
+                for(int x=0; x<output.cols(); x++){
+                	boolean lineOneV=false, lineTwoV=false, lineThreeV=false;
+                	int lineOneL=0, lineTwoL=0, lineThreeL=0;
                 	
-                	if( get3DDistanceSqr(BLUE[0], BLUE[1], BLUE[2], convertColor(pixels[x]), convertColor(pixels[x+1]), convertColor(pixels[x+2])) < 180*180 ){
-                		pixels[x]=pixels[x+1]=pixels[x+2]=0;
-                	}
+                	int temp=0;
                 	
-                }*/
-                System.out.println("pixel value "+pixels[42]);
-                for(int x=0; x<pixels.length; x++){
-                	if(convertColor(pixels[x])>200){
-                		pixels[x]=(byte)255;
-                		pixelValues[(int)x/640][x%640]=1;
-                	}else{
-                		pixels[x]=(byte) 0;
+                	//if true, white
+                	boolean lastLineValue=convertColor(pixels[x])>200;
+                	for(int y=0; y+x<pixels.length; y+=output.cols()){
+	                	///test lines aren't equal spacing
+                		///loop through, if switch color, record line value, test if propotions match
+                		pixels[x+y]=0;
+                		if(lastLineValue!=convertColor(pixels[x+y])>200){
+                			pixels[x+y]=0;
+                			
+                			lineOneL=lineTwoL;
+                			lineTwoL=lineThreeL;
+                			lineThreeL=temp;
+                			
+                			lineOneV=lineTwoV;
+                			lineTwoV=lineThreeV;
+                			lineThreeV=lastLineValue;
+                			
+                			lastLineValue=convertColor(pixels[x+y])>200;
+                			temp=0;
+                			
+                			if(lineOneV==true && lineTwoV==false && lineThreeV==true){
+                				double lineOneRatio=(lineOneL)/(lineOneL+lineTwoL+lineThreeL);
+                				double lineTwoRatio=(lineTwoL)/(lineOneL+lineTwoL+lineThreeL);
+                				if( lineOneRatio>.3 && lineOneRatio<.5 && lineTwoRatio>.3 && lineTwoRatio<.5){
+                					pixels[x+y]=(byte)255;
+                					System.out.println("colum :"+x);
+                				}
+                				
+                				
+                			}
+                		}else{
+                			temp++;
+                		}
                 		
-                	}
                 	
-                	/*where s(x,y)=summed area whatever, i(x.y)=current pixel value s(x,y)
-                	 * =i(x,y)+s(x-1, y)+s(x,y-1)-s(x-1,y-1)
-                	 */
-                	//checks for pixel above
-                	if( ((int)x/640)-1>0){
-                		pixelValues[(int)x/640][x%640]+=pixelValues[((int)x/640) - 1][x%640];
                 	}
-                	//checks for pxiel to the right
-                	if( x%640>0){
-                		pixelValues[(int)x/640][x%640]+=pixelValues[((int)x/640)][(x%640) - 1];
-                	}
-                	//checks for pixel above and to the right
-                	if(x%640>0 && ((int)x/640)-1>0){
-                		pixelValues[(int)x/640][x%640]-=pixelValues[((int)x/640)-1][(x%640) - 1];
-                	}
-                	
-                	System.out.println("image volume" + getIImageVolume(pixelValues[479][639], pixelValues[240][320], pixelValues[240][639], pixelValues[479][320]) );
-                	
-                	
                 		
                 	
                 }
+                
                 
                 //System.out.println(get3DDistance(1,1,1,4,5,6));
                 output.put(0, 0, pixels);
@@ -183,11 +177,13 @@ public class Robot extends IterativeRobot {
                 System.out.print(pixels[2]+" ");
                 System.out.print("\n");
                 */
+                /*
                 if(timerCounter%60==0){
                 	double currentTime=System.currentTimeMillis();
                 	System.out.println( "milis Time per sixty frames: "+String.valueOf( currentTime-lastTime) ); 
                 	lastTime=currentTime;
                 }
+                */
             }
         }).start();
 	}
